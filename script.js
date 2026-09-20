1850b6262f09f5600291e3df6848d60cd70c716b
Vikash Kumar <vikaskumarkxz@gmail.com>
2026-09-21 03:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,48 @@ function convertToRoman(num) {
 
   //your code here
 
+	let result = "";
+
+	while (num > 0) {
+
+    if (num >= 1000) {
+        result = "M" + result;
+		num = num - 1000;
+    }
+    else if (num >= 500) {
+		result = "D" + result;
+		num = num - 500;
+    }
+    else if (num >= 100) {
+		result = "C" + result;
+		num = num - 100;
+    }
+	else if (num >= 50) {
+		result = "L" + result;
+		num = num - 50;
+    }
+	else if (num >= 10) {
+		result = "X" + result;
+		num = num - 10;
+    }
+	else if (num >= 5) {
+		result = "V" + result;
+		num = num - 5;
+    }
+	else if (num >= 1) {
+		result = "I" + result;
+		num = num - 1;
+    }
+
+    // continue for 50, 10, 5, 1
+}
+
+return result;
+
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
